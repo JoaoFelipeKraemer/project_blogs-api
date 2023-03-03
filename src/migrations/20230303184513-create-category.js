a39c609042955f3960b57bf8c68ecb170b9df051
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-03-03 15:54:55 -0300

    fix: add categories migration, req 1

---
+++ src/migrations/20230303184513-create-category.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+     await queryInterface.createTable('Categories', {
+       id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,        
+      },
+       name: {
+        type:Sequelize.INTEGER,
+        allowNull: false,        
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+ 
+
+      await queryInterface.dropTable('Categories');
+
+  }
+};
