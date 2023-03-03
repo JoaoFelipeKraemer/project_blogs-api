9b614a02990c56542dd8ab88db221a4c5c68c2d5
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-03-03 17:46:39 -0300

    feature: create User model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+// src/models/user.model.js
+
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          displayName: DataTypes.STRING,          
+          email: DataTypes.STRING,          
+          password: DataTypes.STRING,          
+          image : DataTypes.STRING,          
+        },
+          {
+            tableName: 'users',
+            timestamps: false,
+            underscored: true,
+          }
+        );
+        User.associate = (models) => {
+            User.hasMany(models.BlogPost,
+             { foreignKey: 'userId', as: 'blog_posts' });
+         };
+    return User; 
+  };
+  
+  module.exports = UserModel;
